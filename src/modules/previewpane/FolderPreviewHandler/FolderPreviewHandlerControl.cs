2070d73bc19cbd776d100141b12105a4af42050e
Callum C <callumc34@hotmail.com>
2022-04-06 11:18:28 +1200

    Added UI for folder previewing

--- src/modules/previewpane/FolderPreviewHandler/FolderPreviewHandlerControl.cs
+++ src/modules/previewpane/FolderPreviewHandler/FolderPreviewHandlerControl.cs
@@ -3,9 +3,15 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Windows.Forms;
 using Common;
+using Microsoft.PowerToys.PreviewHandler.Folder.Properties;
+using Microsoft.PowerToys.PreviewHandler.Folder.Utilities;
 using Microsoft.PowerToys.Telemetry;
+using Windows.UI.ViewManagement;
 
 namespace Microsoft.PowerToys.PreviewHandler.Folder
 {
@@ -14,21 +20,67 @@ namespace Microsoft.PowerToys.PreviewHandler.Folder
     /// </summary>
     public class FolderPreviewHandlerControl : FormHandlerControl
     {
+        private const int SHOWCAP = 20;
+
+        /// <summary>
+        /// Use UISettings to get system colors and scroll bar size.
+        /// </summary>
+        private static UISettings _uISettings = new UISettings();
+
+        private FlowLayoutPanel _layout;
+
         /// <summary>
         /// Start the preview on the Control.
         /// </summary>
         /// <param name="dataSource">Stream reference to access source file.</param>
         public override void DoPreview<T>(T dataSource)
         {
+            SuspendLayout();
             try
             {
-                WebBrowser browser = new WebBrowser();
+                if (!(dataSource is string filePath))
+                {
+                    throw new ArgumentException($"{nameof(dataSource)} for {nameof(FolderPreviewHandler)} must be a string but was a '{typeof(T)}'");
+                }
 
-                browser.DocumentText = "Test";
-                browser.Dock = DockStyle.Fill;
-                browser.IsWebBrowserContextMenuEnabled = true;
-                Controls.Add(browser);
-                PowerToysTelemetry.Log.WriteEvent(new Telemetry.Events.FolderPreviewed());
+                InvokeOnControlThread(() =>
+                {
+                    BackColor = GetThemeColor(UIColorType.Background);
+                    Resize += FormResized;
+
+                    _layout = new FlowLayoutPanel
+                    {
+                        AutoScroll = true,
+                        AutoSize = true,
+                        Dock = DockStyle.Fill,
+                        FlowDirection = FlowDirection.TopDown,
+                        WrapContents = false,
+                    };
+                    _layout.Width = Width;
+                    _layout.Height = Height;
+
+                    string[] dirList = Directory.GetFileSystemEntries(filePath);
+
+                    int current = 0;
+                    foreach (string item in dirList)
+                    {
+                        if (current >= SHOWCAP)
+                        {
+                            string excessFiles = string.Format(CultureInfo.InvariantCulture, Resources.FolderExtraFilesHidden, dirList.Length - SHOWCAP);
+                            _layout.Controls.Add(new ExplorerFile(GetIcon(filePath), excessFiles));
+                            break;
+                        }
+
+                        string[] splitPath = item.Split("\\");
+                        _layout.Controls.Add(new ExplorerFile(GetIcon(item), splitPath[splitPath.Length - 1]));
+                        current++;
+                    }
+
+                    Controls.Add(_layout);
+
+                    // Setup initial textbox sizes.
+                    FormResized(null, null);
+                });
             }
 #pragma warning disable CA1031 // Do not catch general exception types
             catch (Exception ex)
@@ -39,6 +91,112 @@ namespace Microsoft.PowerToys.PreviewHandler.Folder
             finally
             {
                 base.DoPreview(dataSource);
+            }
+
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void FormResized(object sender, EventArgs e)
+        {
+            SuspendLayout();
+            _layout.SuspendLayout();
+            _layout.Width = Width;
+            foreach (ExplorerFile panel in _layout.Controls.Find("File", false))
+            {
+                panel.SuspendLayout();
+                panel.Width = Width;
+                panel.UpdateTextBox();
+                panel.ResumeLayout(false);
+            }
+
+            _layout.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        /// <summary>
+        /// Get the system theme color, based on the selected theme.
+        /// </summary>
+        /// <returns>An object of type <see cref="Color"/>.</returns>
+        private static Color GetThemeColor(UIColorType type)
+        {
+            var sysColor = _uISettings.GetColorValue(type);
+
+            // Windows dark theme is set to RGB(0,0,0) which does not match properly
+            if (type == UIColorType.Background && sysColor.R == 0)
+            {
+                return Color.FromArgb(255, 32, 32, 32);
+            }
+
+            return Color.FromArgb(sysColor.A, sysColor.R, sysColor.G, sysColor.B);
+        }
+
+        private static Icon GetIcon(string path)
+        {
+            FileAttributes attr = File.GetAttributes(path);
+
+            if (attr.HasFlag(FileAttributes.Directory))
+            {
+                return FolderIconHelper.FolderLarge;
+            }
+
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            Icon fileIcon = Icon.ExtractAssociatedIcon(path);
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+            if (fileIcon == null)
+            {
+                return SystemIcons.WinLogo;
+            }
+
+            return fileIcon;
+        }
+
+        private class ExplorerFile : FlowLayoutPanel
+        {
+            private PictureBox _pictureBox;
+            private TextBox _textBox;
+
+            private Icon _icon;
+            private string _file;
+
+            public ExplorerFile(Icon icon, string file)
+            {
+                _icon = icon;
+                _file = file;
+
+                Name = "File";
+                Dock = DockStyle.Fill;
+                AutoSize = true;
+                FlowDirection = FlowDirection.LeftToRight;
+
+                _pictureBox = new PictureBox
+                {
+                    Dock = DockStyle.Fill,
+                    Image = _icon.ToBitmap(),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    AutoSize = true,
+                    Padding = Padding.Empty,
+                };
+
+                _textBox = new TextBox
+                {
+                    Text = _file,
+                    BackColor = FolderPreviewHandlerControl.GetThemeColor(UIColorType.Background),
+                    ForeColor = FolderPreviewHandlerControl.GetThemeColor(UIColorType.Foreground),
+                    ReadOnly = true,
+                    BorderStyle = BorderStyle.None,
+                };
+
+                // Apply margin after text is added
+                _textBox.Margin = new Padding(0, (_pictureBox.Height - _textBox.Height) / 2, 0, 0);
+
+                Controls.Add(_pictureBox);
+                Controls.Add(_textBox);
+            }
+
+            public void UpdateTextBox()
+            {
+                _textBox.Width = (int)(0.6 * Width);
             }
         }
     }
---
+++ src/modules/previewpane/FolderPreviewHandler/Utilities/FolderIconHelper.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.PowerToys.PreviewHandler.Folder.Utilities
+{
+    /// <summary>
+    /// Helper class to get a folder icon
+    /// Code courtesy of <see href="https://stackoverflow.com/a/59129804">Stack Overflow</see>
+    /// Author: <see href="https://stackoverflow.com/users/574531/herohtar">Herohtar</see>
+    /// </summary>
+    public static class FolderIconHelper
+    {
+        private static Icon folderIcon;
+
+        /// <summary>
+        /// Gets default windows folder icon
+        /// </summary>
+        public static Icon FolderLarge => folderIcon ?? (folderIcon = GetStockIcon(SHSIIDFOLDER, SHGSILARGEICON));
+
+        private static Icon GetStockIcon(uint type, uint size)
+        {
+            var info = default(SHSTOCKICONINFO);
+            info.cbSize = (uint)Marshal.SizeOf(info);
+
+            _ = SHGetStockIconInfo(type, SHGSIICON | size, ref info);
+
+            var icon = (Icon)Icon.FromHandle(info.hIcon).Clone(); // Get a copy that doesn't use the original handle
+            DestroyIcon(info.hIcon); // Clean up native icon to prevent resource leak
+
+            return icon;
+        }
+
+        /// <summary>
+        /// Stock icon information struct
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct SHSTOCKICONINFO
+        {
+#pragma warning disable SA1307 // Accessible fields must begin with upper case letter
+            public uint cbSize;
+            public IntPtr hIcon;
+            public int iSysIconIndex;
+            public int iIcon;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szPath;
+#pragma warning restore SA1307 // Accessible fields must begin with upper case letter
+        }
+
+        /// <summary>
+        /// Retrieves information about system-defined shell icons.
+        /// </summary>
+        /// <param name="siid">Stock icon ID</param>
+        /// <param name="uFlags">Flags</param>
+        /// <param name="psii">Returned stock icon info</param>
+        /// <returns>S_OK if successful else HRESULT error code</returns>
+        [DllImport("shell32.dll")]
+        private static extern int SHGetStockIconInfo(uint siid, uint uFlags, ref SHSTOCKICONINFO psii);
+
+        /// <summary>
+        /// Destroys an icon and frees any memory the icon occupied.
+        /// </summary>
+        /// <param name="handle">Handle icon to destroy</param>
+        /// <returns>If the function succeeds the return value is non zero</returns>
+        [DllImport("user32.dll")]
+        private static extern bool DestroyIcon(IntPtr handle);
+
+        private const uint SHSIIDFOLDER = 0x3;
+        private const uint SHGSIICON = 0x100;
+        private const uint SHGSILARGEICON = 0x0;
+    }
+}
